2bda08527d56f99c933df3d8e9fbb6a88212b35e
LAPTOP-0UL5E3M3\ruben <rubenchod22@gmail.com>
2021-10-02 06:24:00 -0500

    SP2-HU008 se adicionan pantallas de visualización de articulos y elementos adicionados al carrito

---
+++ tiendavirtual/src/datos/Articulos.js
@@ -0,0 +1,12 @@
+export  const articulos = [
+    {nombre:'Sacapuntas', valor: 300, detalle: 'Marca W', imagen:"/imagenes/lapiz.jpg", idElemento:4},
+    {nombre:'Cuaderno', valor: 1000, detalle: 'Marca Genérica', imagen:"/imagenes/cuaderno.jpg", idElemento:1},
+    {nombre:'Lápiz', valor: 500, detalle: 'Marca W',  imagen:"/imagenes/lapiz.jpg", idElemento:2},
+    {nombre:'Borrador', valor: 200, detalle: 'Marca Genérica', imagen:"/imagenes/lapiz.jpg", idElemento:3},
+   
+    {nombre:'Regla', valor: 800, detalle: 'Marca Genérica', imagen:"/imagenes/lapiz.jpg", idElemento:5},
+    {nombre:'Block', valor: 2000, detalle: 'Marca W', imagen:"/imagenes/lapiz.jpg", idElemento:6},
+    {nombre:'Colores', valor: 25000, detalle: 'Marca Genérica',imagen:"/imagenes/lapiz.jpg", idElemento:7},
+    {nombre:'Crayolas', valor: 3000, detalle: 'Marca W', imagen:"/imagenes/lapiz.jpg", idElemento:8}
+
+];
--- tiendavirtual/src/index.js
+++ tiendavirtual/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ArticulosVenta from './componentes/ArticulosVenta';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <Provider store={store}>
+    <ArticulosVenta />
+    </Provider>,
+
   document.getElementById('root')
 );
 

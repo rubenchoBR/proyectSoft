46d9bf230a5ac190d5d49b8da26c9ff27b8d471d
pedroluis <pedroacostaperez@gmail.com>
2021-10-14 17:36:47 -0500

    Login update

--- frontend-tienda/src/components/modulos/login/Login.jsx
+++ frontend-tienda/src/components/modulos/login/Login.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 import './loginStyle.css';
 
 const Login = () => {
+
+    const history = useHistory();
+    const [formValues, setFormValues] = useState({})
+
+    const changeField = (e) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const submit = (e) => { 
+        e.preventDefault();
+        history.push('/verArticulos')
+    }
+
     return (
-        <div className="login">
-            
+        <div className="login">            
             <div className="login-form ">
                 <h1>GigaDevs Shop</h1>
+                <form>                
                 <div className="form-group ">
-                    <input type="email" className="form-control" placeholder="Nombre de usuario" id="UserName" />
+                    <input onChange={changeField} value={formValues.username} name="username" type="email" className="form-control" placeholder="Nombre de usuario" id="UserName" />
                     <i class="fa fa-user"></i>
                 </div>
                 <div className="form-group log-status">
-                    <input type="password" className="form-control" placeholder="Contraseña" id="Password"/>
+                    <input onChange={changeField} value={formValues.password} name="password" type="password" className="form-control" placeholder="Contraseña" id="Password"/>
                     <i class="fa fa-lock"></i>
                 </div>
                 <a className="link" href="#">¿Olvidaste tu contraseña?</a>
-                <button type="button" className="log-btn" >Ingresar</button>
+                <button type="submit" onClick={submit} type="button" className="log-btn" >Ingresar</button>          
+                </form>
             </div>
             
         </div>
